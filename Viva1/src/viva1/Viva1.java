a0b7aa71fb0fd5c95477e021d07a4ba34a408832
Kristern <147322072+kristern1257@users.noreply.github.com>
2024-01-28 21:06:15 +0800

    Viva1.java

--- Viva1/src/viva1/Viva1.java
+++ Viva1/src/viva1/Viva1.java
@@ -8,46 +8,46 @@ import java.util.Scanner;
 public class Viva1 {
     public static void main(String[] args) {
         //question1//
-//        System.out.println("    1    ");
-//        System.out.println("   212   ");
-//        System.out.println("  32123  ");
-//        System.out.println(" 4321234 ");
-//        System.out.println("543212345");
-//
-//        int n = 5;                //initialise the number of rows
-//
-//        for (int i = 1; i <= n; i++) {//outer loop to control the number of rows in the pattern, iterating from 1 to n (
-//
-//            for (int j = 1; j <= n - i; j++) {//This inner loop prints spaces BEFORE the numbers. The number of spaces decreases as you move down the rows. It prints n - i spaces in each row.
-//                System.out.print(" ");
-//            }
-//
-//            for (int j = i; j >= 1; j--) {// This loop prints the numbers in decreasing order from i to 1.
-//                System.out.print(j);
-//            }
-//            for (int j = 2; j <= i; j++) { //This loop prints the numbers in increasing order from 2 to i.
-//                System.out.print(j);
-//            }
-//            System.out.println();       //move to the next line in the output.
-//
-//        }
+        System.out.println("    1    ");
+        System.out.println("   212   ");
+        System.out.println("  32123  ");
+        System.out.println(" 4321234 ");
+        System.out.println("543212345");
+
+        int n = 5;                //initialise the number of rows
+
+        for (int i = 1; i <= n; i++) {//outer loop to control the number of rows in the pattern, iterating from 1 to n (
+
+            for (int j = 1; j <= n - i; j++) {//This inner loop prints spaces BEFORE the numbers. The number of spaces decreases as you move down the rows. It prints n - i spaces in each row.
+                System.out.print(" ");
+            }
+
+            for (int j = i; j >= 1; j--) {// This loop prints the numbers in decreasing order from i to 1.
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i; j++) { //This loop prints the numbers in increasing order from 2 to i.
+                System.out.print(j);
+            }
+            System.out.println();       //move to the next line in the output.
+
+        }
         
         
         //question2//
-//        Scanner k = new Scanner(System.in);
-//        System.out.print("Enter an integer: ");
-//        String integer = k.nextLine();
-//        int count = integer.length();
-//        System.out.print("Reversed integer: ");
-//        if (integer.charAt(0)=='-'){
-//            System.out.print("-");
-//            for (int i=count-1; i>0; i--)   //not print index 0  becuz index 0 is '-'
-//            System.out.print(integer.charAt(i)); 
-//        }
-//        else{
-//        for (int i=count-1; i>=0; i--)   
-//        System.out.print(integer.charAt(i)); 
-//        }
+        Scanner k = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        String integer = k.nextLine();
+        int count = integer.length();
+        System.out.print("Reversed integer: ");
+        if (integer.charAt(0)=='-'){
+            System.out.print("-");
+            for (int i=count-1; i>0; i--)   //not print index 0  becuz index 0 is '-'
+            System.out.print(integer.charAt(i)); 
+        }
+        else{
+        for (int i=count-1; i>=0; i--)   
+        System.out.print(integer.charAt(i)); 
+        }
 
       //queation2 extra//
 //        Scanner sc= new Scanner(System.in);
@@ -75,31 +75,31 @@ public class Viva1 {
         /* Compose a program to check whether a number entered by a user is a Strong Number or not. 
         A Strong Number is a special number for which the sum of the factorials of each digit 
         is equal to the original number.*/
-//        Scanner sc= new Scanner(System.in);
-//        System.out.print("Enter an integer : ");
-//        int oriNum=sc.nextInt();
-//        int num = oriNum,sum=0, digit;
-//        
-//        do{
-//            digit = num%10;
-//            int factorial = 1;
-//            for(int i=1; i<=digit;i++){
-//                factorial*=i;
-//            }
-//            sum+=factorial;
-//            System.out.println(digit+"! is "+factorial);
-//            num/=10;
-//        }while(num>0);
-//        
-//        System.out.println("\nSum of factorial : "+sum);
-//        if(sum==oriNum)
-//            System.out.println(oriNum+" is a Strong Number.");
-//        else
-//            System.out.println(oriNum+" is not a Strong Number.");
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter an integer : ");
+        int oriNum=sc.nextInt();
+        int num = oriNum,sum=0, digit;
+        
+        do{
+            digit = num%10;
+            int factorial = 1;
+            for(int i=1; i<=digit;i++){
+                factorial*=i;
+            }
+            sum+=factorial;
+            System.out.println(digit+"! is "+factorial);
+            num/=10;
+        }while(num>0);
+        
+        System.out.println("\nSum of factorial : "+sum);
+        if(sum==oriNum)
+            System.out.println(oriNum+" is a Strong Number.");
+        else
+            System.out.println(oriNum+" is not a Strong Number.");
         
         
         //question4//
-        Scanner k = new Scanner(System.in);
+//        Scanner k = new Scanner(System.in);
         System.out.print("Enter your password: ");
         String password = k.nextLine();
         
